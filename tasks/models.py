f155e2df1a7b85f78b6b422d5bd5117fa9e07772
Santiago <chesslike693@gmail.com>
2023-07-20 00:08:08 -0400

    Com

--- tasks/models.py
+++ tasks/models.py
@@ -4,12 +4,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Task(models.Model):
-  title = models.CharField(max_length=200)
-  description = models.TextField(max_length=1000)
-  created = models.DateTimeField(auto_now_add=True)
-  datecompleted = models.DateTimeField(null=True, blank=True)
-  important = models.BooleanField(default=False)
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Fields of the Task model
+    title = models.CharField(max_length=200)  # CharField for the task title with a maximum length of 200 characters.
+    description = models.TextField(max_length=1000)  # TextField for the task description with a maximum length of 1000 characters.
+    created = models.DateTimeField(auto_now_add=True)  # DateTimeField to store the creation date and time of the task. It is automatically set to the current date and time when the object is created.
+    datecompleted = models.DateTimeField(null=True, blank=True)  # DateTimeField to store the completion date and time of the task. It can be left empty (null=True) and can be shown as an empty string in the template (blank=True).
+    important = models.BooleanField(default=False)  # BooleanField to indicate whether the task is important or not. It is set to False by default.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # ForeignKey to link the task with a user. If a user is deleted, all related tasks will also be deleted (on_delete=models.CASCADE).
 
-  def __str__(self):
-    return self.title + ' - ' + self.user.username
+    # Method to display a string representation of the Task object.
+    def __str__(self):
+        return self.title + ' - ' + self.user.username
+
--- tasks/views.py
+++ tasks/views.py
@@ -11,16 +11,19 @@ from .forms import TaskForm
 
 # Create your views here.
 
-
+# Function for user signup
 def signup(request):
+    '''Handles user signup. If the request method is 'GET', renders the signup.html template
+    with the UserCreationForm. If the request method is 'POST', creates a new user with the
+    provided username and password, and logs in the user. If there's an error, renders the
+    signup.html template with an appropriate error message.'''
     if request.method == 'GET':
         return render(request, 'signup.html', {"form": UserCreationForm})
     else:
-
+        # Handle POST request for user signup
         if request.POST["password1"] == request.POST["password2"]:
             try:
-                user = User.objects.create_user(
-                    request.POST["username"], password=request.POST["password1"])
+                user = User.objects.create_user(request.POST["username"], password=request.POST["password1"])
                 user.save()
                 login(request, user)
                 return redirect('tasks')
@@ -30,19 +33,31 @@ def signup(request):
         return render(request, 'signup.html', {"form": UserCreationForm, "error": "Passwords did not match."})
 
 
+# Function to display the tasks for the logged-in user
 @login_required
 def tasks(request):
+    '''Retrieves all tasks for the logged-in user that are not completed (datecompleted is null).
+    Renders the tasks.html template with the retrieved tasks.'''
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'tasks.html', {"tasks": tasks})
 
+
+# Function to display the completed tasks for the logged-in user
 @login_required
 def tasks_completed(request):
+    '''Retrieves all completed tasks for the logged-in user and orders them by datecompleted in
+    descending order. Renders the tasks.html template with the retrieved completed tasks.'''
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
     return render(request, 'tasks.html', {"tasks": tasks})
 
 
+# Function to create a new task for the logged-in user
 @login_required
 def create_task(request):
+    '''Handles the creation of a new task for the logged-in user. If the request method is 'GET',
+    renders the create_task.html template with an empty TaskForm to create a new task. If the
+    request method is 'POST', saves the new task associated with the logged-in user. If there's an
+    error, renders the create_task.html template with an appropriate error message.'''
     if request.method == "GET":
         return render(request, 'create_task.html', {"form": TaskForm})
     else:
@@ -56,30 +71,46 @@ def create_task(request):
             return render(request, 'create_task.html', {"form": TaskForm, "error": "Error creating task."})
 
 
+# Function to display the home page
 def home(request):
+    '''Renders the home.html template.'''
     return render(request, 'home.html')
 
 
+# Function to sign out the current user
 @login_required
 def signout(request):
+    '''Logs out the current user and redirects to the home page.'''
     logout(request)
     return redirect('home')
 
 
+# Function for user sign-in
 def signin(request):
+    '''Handles user sign-in. If the request method is 'GET', renders the signin.html template
+    with the AuthenticationForm to log in. If the request method is 'POST', authenticates the
+    user with the provided username and password. If the authentication fails, renders the
+    signin.html template with an appropriate error message. If successful, logs in the user and
+    redirects to the tasks page.'''
     if request.method == 'GET':
         return render(request, 'signin.html', {"form": AuthenticationForm})
     else:
-        user = authenticate(
-            request, username=request.POST['username'], password=request.POST['password'])
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
             return render(request, 'signin.html', {"form": AuthenticationForm, "error": "Username or password is incorrect."})
 
         login(request, user)
         return redirect('tasks')
 
+
+# Function to view the details of a specific task for the logged-in user
 @login_required
 def task_detail(request, task_id):
+    '''Handles the viewing and updating of a specific task for the logged-in user. If the request
+    method is 'GET', retrieves the task with the given task_id that belongs to the logged-in user.
+    Renders the task_detail.html template with the retrieved task and its TaskForm. If the request
+    method is 'POST', updates the task details and redirects to the tasks page. If there's an
+    error, renders the task_detail.html template with an appropriate error message.'''
     if request.method == 'GET':
         task = get_object_or_404(Task, pk=task_id, user=request.user)
         form = TaskForm(instance=task)
@@ -93,16 +124,29 @@ def task_detail(request, task_id):
         except ValueError:
             return render(request, 'task_detail.html', {'task': task, 'form': form, 'error': 'Error updating task.'})
 
+
+# Function to mark a specific task as completed for the logged-in user
+
+
 @login_required
 def complete_task(request, task_id):
+    '''Handles marking a specific task with the given task_id as completed for the logged-in user.
+    Retrieves the task with the provided task_id that belongs to the logged-in user. If the request
+    method is 'POST', sets the completion date to the current date and time, saves the updated task,
+    and redirects to the tasks page.'''
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
         task.datecompleted = timezone.now()
         task.save()
         return redirect('tasks')
 
+
+# Function to delete a specific task for the logged-in user
 @login_required
 def delete_task(request, task_id):
+    '''Handles deleting a specific task with the given task_id for the logged-in user. Retrieves the
+    task with the provided task_id that belongs to the logged-in user. If the request method is 'POST',
+    deletes the task and redirects to the tasks page.'''
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
         task.delete()
